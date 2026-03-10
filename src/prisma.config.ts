02409c143e1f0c742b33f34cb7236bbd87cc47c5
anshshr <anshshrivastav24@gmail.com>
2026-03-10 17:22:07 +0530

    fix(postgres) : fixed the postgres datasource

--- src/prisma.config.ts
+++ src/prisma.config.ts
@@ -7,6 +7,6 @@ export default defineConfig({
     path: "prisma/migrations",
   },
   datasource: {
-    url: "postgresql://neondb_owner:npg_NKY6tvPbwJ5Z@ep-noisy-wave-a1bhpajr-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=verify-full",
+    url: process.env.DATABASE_URL!,
   },
 });
