e18c65c28bd9d400952fce9ed3ae2db12ec65478
anshshr <anshshrivastav24@gmail.com>
2026-03-18 16:24:32 +0530

    feat(notifications) : Added Dead Letter Queue(DLQ) for the failed jobs

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -1,15 +1,17 @@
 // Require the Bull library
 import Queue from "bull";
 
-// Define Redis connection details
-const redisConfig = {
-  host: "127.0.0.1", // Redis server address
-  port: 6379, // Redis server port
-  // You can add more Redis connection options here if needed
-};
-
 // Create a queue with the specified Redis connection
-const notificationQueue = new Queue("notifications", { redis: redisConfig });
+export const notificationQueue = new Queue("notifications", {
+  redis: {
+    host: "127.0.0.1",
+    port: 6379,
+  },
+});
 
-
-export default notificationQueue;
+export const DLQ = new Queue("DLQ", {
+  redis: {
+    host: "127.0.0.1",
+    port: 6379,
+  },
+});
