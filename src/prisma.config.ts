43582c8699510bf73b4fe50176a6568c704463a4
anshshr <anshshrivastav24@gmail.com>
2026-03-29 15:15:51 +0530

    fix : fix the .env issue

--- src/prisma.config.ts
+++ src/prisma.config.ts
@@ -2,7 +2,6 @@ import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
 const URL = process.env.DATABASE_URL;
-console.log(URL);
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
-import "dotenv/config.js";
+import dotenv from "dotenv";
 import app from "./app.js";
+import { dot } from "node:test/reporters";
+dotenv.config();
 
-const PORT =  3001;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`server is runing in the Port ${PORT}`);
